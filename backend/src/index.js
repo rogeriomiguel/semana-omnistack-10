a9f534c4d4b1e22dc4b1f4b02cae1fd6c6bb28d9
Rogerio Miguel <rogerio.miguel1@hotmail.com>
2020-04-10 14:14:34 -0300

    Funcionalidades Avançadas

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
 const cors = require('cors')
+const http = require('http')
+const { setupWebsocket } = require('./websocket')
 
 const app = express()
+const server = http.Server(app)
+
+setupWebsocket(server)
 
 mongoose.connect('mongodb+srv://root:root@cluster0-47zn4.mongodb.net/week10?retryWrites=true&w=majority',   {
   useNewUrlParser: true,
@@ -16,4 +21,4 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333)
+server.listen(3333)
